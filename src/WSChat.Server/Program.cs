9510c24bf7350bd5081de3e790d1006b0f491af4
JulioBorges <borges.juliocezar@gmail.com>
2021-07-05 09:20:15 -0300

    Revert "Development"

--- src/WSChat.Server/Program.cs
+++ src/WSChat.Server/Program.cs
@@ -1,38 +1,12 @@
 ﻿using System;
-using WSChat.Shared;
-using WSChat.Shared.Helpers;
 
 namespace WSChat.Server
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            const string title = "CHAT SERVER";
-            (string ip, int port) = ConsoleHelper.LoadOrShowConfigurationMenu(args);
-            ConsoleHelper.PrintHeaderChat(ip, port, title);
-
-            ChatServer chatServer = new();
-
-            chatServer.SetLogHandler(log => {
-                Console.WriteLine($"[LOG]: {log}");
-            });
-
-            chatServer.SetErrorHandler(error => {
-                Console.WriteLine($"[ERROR]: {error.Message}");
-                Console.WriteLine();
-            });
-
-            chatServer.StartServer(ip, port);
-
-            if (chatServer.Running)
-            {
-                chatServer.StartThreads();
-                Console.Read();
-                chatServer.StopServer();
-            }
-            else
-                Console.WriteLine("Server failure");
+            Console.WriteLine("Hello World!");
         }
     }
 }
