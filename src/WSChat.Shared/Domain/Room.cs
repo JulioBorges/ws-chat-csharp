278f5ee6fba1634beeb2ac2fddc6929821dc06cf
JulioBorges <borges.juliocezar@gmail.com>
2021-07-05 15:24:26 -0300

    fixing merge request errors.

---
+++ src/WSChat.Shared/Domain/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WSChat.Shared.Domain
+{
+    public class Room : IComparable<Room>
+    {
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Room(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+        }
+
+        public int CompareTo(Room other) => (Name == other.Name) ? 0 : 1;
+    }
+}
---
+++ src/WSChat.Shared/Enums/MessageType.cs
@@ -0,0 +1,14 @@
+﻿namespace WSChat.Shared.Enums
+{
+    public enum MessageType
+    {
+        Register,
+        Quit,
+        JoinRoom, 
+        QuitRoom,
+        SendMessage,
+        SendPrivateMessage,
+        ListChatRooms,
+        ListUsers
+    }
+}
